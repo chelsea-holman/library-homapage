71fca4e2895128f5caed7960eecb1ebdcc2ec118
ladylaoban <65156306+ladylaoban@users.noreply.github.com>
2021-04-29 11:36:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 
+
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); 
 }
 
-    //<![CDATA[
+
+
+
+  
     function carouselfixes() {
         var carouselcontainer = document.getElementsByClassName("EBSCOcarouselContainer");
         if (carouselcontainer.length > 0) {
             for (var ebsco = 0; ebsco < carouselcontainer.length; ebsco++) {
-                carouselcontainer[ebsco].style.display = "none"; // For Chrome v44 xsl iframe bug.
-                carouselcontainer[ebsco].style.display = "block"; // For Chrome v44 xsl iframe bug.
-                if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) { //check if using iOS mobile
+                carouselcontainer[ebsco].style.display = "none"; 
+                carouselcontainer[ebsco].style.display = "block"; 
+                if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) { 
                     carouselcontainer[ebsco].scrolling = "no";
                     
                 }
@@ -29,5 +38,6 @@ function showSlides() {
         }
     }
     window.addEventListener("load", carouselfixes(), false);
-    //]]>
+  
+ 
  
